1de41e50a4971309f09fc3702bd1d1d3fe509bcb
Mateusz Kussowski <mateusz.kussowski@icloud.com>
2025-03-25 17:17:25 +0100

    hello

--- bot/main.py
+++ bot/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    print("hello world!")
+#write to tmp.txt
+def write_to_file():
+    with open('tmp.txt', 'w') as f:
+        f.write('Hello World')
+
+if __name__ == '__main__':
+    write_to_file()
